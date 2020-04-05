22ce814c00cbc3b0269c0d849d706f4be6fa470b
Davi Queiroz <daviqueiroz92@gmail.com>
2020-04-05 00:46:27 -0300

    Adding Character Builder

--- src/main/java/com/marvelcharacters/domain/Character.java
+++ src/main/java/com/marvelcharacters/domain/Character.java
@@ -1,11 +1,13 @@
 package com.marvelcharacters.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -18,7 +20,8 @@ public class Character {
     public String id;
     public String name;
     public String description;
-    public String modified;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    public LocalDateTime modified;
     public Thumbnail thumbnail;
     public String resourceURI;
     public Comics comics;
